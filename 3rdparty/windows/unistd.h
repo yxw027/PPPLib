b0dff7ef8e0966daae3e4fa2b1c502db5b8338ce
heiwa0519 <cchen@cumt.edu.cn>
2020-09-13 16:14:18 +0800

    first commit

---
+++ 3rdparty/windows/unistd.h
@@ -0,0 +1,10 @@
+/** This file is part of the Mingw32 package. 
+*  unistd.h maps     (roughly) to io.h 
+*/  
+#pragma once
+
+#ifndef _UNISTD_H  
+#define _UNISTD_H  
+#include <io.h>  
+#include <process.h>  
+#endif /* _UNISTD_H */
